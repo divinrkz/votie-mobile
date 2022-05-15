387e7b6847282852c1abd38b3e7d37c84e121583
dushimsam <samueldushimimana95@gmail.com>
2022-05-15 12:22:20 +0200

    created folder screens

---
+++ screens/searchPage.js
@@ -0,0 +1,27 @@
+const SearchPage = () => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5761A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input:{
+    height: 40,
+    marginTop: 20,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius:30
+  }
+});
