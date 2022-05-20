9565e73deb923024bd8cc95f3c6a83e0b9f931dc
DivinIrakiza <irakizadivin@gmail.com>
2022-05-20 21:04:56 +0200

    feat: search screen

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,26 +1,54 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
-
+import { FontAwesome, Feather } from "@expo/vector-icons";
+import { View, Text, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function SearchScreen() {
-  return (
-        <View style={styles.container}>
-            <Text>Search Screen</Text>
-        </View>
+    return (
+        <SafeAreaView
+            style={{
+                backgroundColor: "#F7941D",
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "space-evenly",
+            }}
+        >
+            <View
+                style={{
+                    backgroundColor: "white",
+                    flexDirection: "row",
+                    padding: 5,
+                    borderRadius: 15,
+                }}
+            >
+                <Feather name="search" size={24} color={'#000'} />
+                <TextInput placeholder="Search Your Prefeered Restaurant" />
+            </View>
+            <View>
+                <Text
+                    style={{
+                        fontSize: 24,
+                        fontWeight: "600",
+                        marginTop: 50,
+                    }}
+                >
+                    Or
+                </Text>
+            </View>
+            <View
+                style={{
+                    alignItems: "center",
+                }}
+            >
+                <FontAwesome name="qrcode" size={100} color="black" />
+                <Text
+                    style={{
+                        fontSize: 24,
+                        fontWeight: "600",
+                        marginTop: 50,
+                    }}
+                >
+                    Scan Pay & Enjoy
+                </Text>
+            </View>
+        </SafeAreaView>
     );
-};
-
-
-const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        height: '100%',
-        width: '100%',
-        backgroundColor: '#F7941D',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    textLogo: {
-        fontSize: 24
-    }
-});
+}
